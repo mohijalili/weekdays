58aba2b446a10e23d09ce388e83740c877810c5d
Mohi Jalili <mohijalili@icloud.com>
2021-07-06 17:30:03 +0800

    feat(web): added week module in home

--- libs/web/shell/ui/day/src/lib/day.module.ts
+++ libs/web/shell/ui/day/src/lib/day.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as dayComponents from './components';
 @NgModule({
   imports: [CommonModule],
+  declarations: [...dayComponents.COMPONENTS],
+  exports: [...dayComponents.COMPONENTS],
 })
 export class DayModule {}
