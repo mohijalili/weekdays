436c02643497035f5bbed14a0ad69236c3722096
Mohi Jalili <mohijalili@icloud.com>
2021-07-07 00:12:55 +0800

    feat(ui): added mockup for weekdays

--- libs/web/shell/ui/day/src/lib/components/day/day.component.ts
+++ libs/web/shell/ui/day/src/lib/components/day/day.component.ts
@@ -1,9 +1,12 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
+import { Calendar } from '@study-smarter/web/shared/data-access/models';
 @Component({
   selector: 'study-smarter-day',
   templateUrl: './day.component.html',
   styleUrls: ['./day.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DayComponent {}
+export class DayComponent {
+  @Input() day!: string | Calendar.WeekDay;
+}
