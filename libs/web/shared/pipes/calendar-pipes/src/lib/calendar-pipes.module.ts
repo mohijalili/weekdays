ec9b4184da8324fdc198efd495ee26bd9cbdf440
Mohi Jalili <mohijalili@icloud.com>
2021-07-07 11:29:12 +0800

    feat(shared): make pipes lib builable

--- libs/web/shared/pipes/calendar-pipes/src/lib/calendar-pipes.module.ts
+++ libs/web/shared/pipes/calendar-pipes/src/lib/calendar-pipes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ActiveDayPipe } from './active-day.pipe';
+
 @NgModule({
   imports: [CommonModule],
   declarations: [ActiveDayPipe],
