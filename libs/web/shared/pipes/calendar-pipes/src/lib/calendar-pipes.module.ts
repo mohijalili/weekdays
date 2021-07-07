dad6961c576c8b56e10030dcbe9911bfc9cf715e
Mohi Jalili <mohijalili@icloud.com>
2021-07-07 11:17:22 +0800

    feat(shared): added active day pipe

--- libs/web/shared/pipes/calendar-pipes/src/lib/calendar-pipes.module.ts
+++ libs/web/shared/pipes/calendar-pipes/src/lib/calendar-pipes.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ActiveDayPipe } from './active-day.pipe';
 @NgModule({
   imports: [CommonModule],
+  declarations: [ActiveDayPipe],
+  exports: [ActiveDayPipe],
 })
 export class CalendarPipesModule {}
