026b6f0229d9a7e3f76075f3a628f8e7cc5de250
Mohi Jalili <mohijalili@icloud.com>
2021-07-07 11:01:21 +0800

    feat(web): redorder by day number

--- libs/web/shell/ui/week/src/lib/week.module.ts
+++ libs/web/shell/ui/week/src/lib/week.module.ts
@@ -4,8 +4,10 @@ import { CommonModule } from '@angular/common';
 import * as weekComponents from './components';
 
 import { DayModule } from '@study-smarter/web/shell/ui/day';
+
+import { SortPipesModule } from '@study-smarter/web/shared/pipes/sort-pipes';
 @NgModule({
-  imports: [CommonModule, DayModule],
+  imports: [CommonModule, DayModule, SortPipesModule],
   declarations: [...weekComponents.COMPONENTS],
   exports: [...weekComponents.COMPONENTS],
 })
